61b2ba6bafd64c604dc01f866328fbfd64c8460b
vojan <serejah.mashichev@gmail.com>
2024-05-28 10:09:59 +0300

    Fix http handlers

--- internal/http/handler/handler.go
+++ internal/http/handler/handler.go
@@ -3,7 +3,10 @@ package handler
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"net/http"
+	"slices"
 
 	"github.com/Vojan-Najov/daec/internal/service"
 )
@@ -45,19 +48,61 @@ func Decorate(next http.Handler, ds ...Decorator) http.Handler {
 }
 
 // Добавление вычисления арифметического выражения
-func (ls *calcStates) calculate(w http.ResponseWriter, r *http.Request) {
-	//worldState := ls.LifeService.NewState()
+func (cs *calcStates) calculate(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
 
-	//err := json.NewEncoder(w).Encode(worldState.Cells)
-	//if err != nil {
-	//	http.Error(w, err.Error(), http.StatusInternalServerError)
-	//}
+	if !slices.Contains(r.Header["Content-Type"], "application/json") {
+		http.Error(w, "Incorrect header", http.StatusUnprocessableEntity)
+		return
+	}
+
+	type Expression struct {
+		Id         string `json:"id"`
+		Expression string `json:"expression"`
+	}
+	var expr Expression
+	err := json.NewDecoder(r.Body).Decode(&expr)
+	if err != nil {
+		fmt.Printf("calculate: %v\n", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if err = cs.CalcService.AddExpression(expr.Id, expr.Expression); err != nil {
+		http.Error(w, err.Error(), http.StatusUnprocessableEntity)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
 }
 
-func (ls *calcStates) listAll(w http.ResponseWriter, r *http.Request) {
+func (cs *calcStates) listAll(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+
+	lst := cs.CalcService.ListAll()
+	encoder := json.NewEncoder(w)
+	encoder.SetIndent("", "    ")
+	err := encoder.Encode(&lst)
+	if err != nil {
+		fmt.Printf("listAll: %v\n", err)
+	}
 }
 
-func (ls *calcStates) listByID(w http.ResponseWriter, r *http.Request) {
+func (cs *calcStates) listByID(w http.ResponseWriter, r *http.Request) {
+	id := r.PathValue("id")
+	expr, err := cs.CalcService.FindById(id)
+	if err != nil {
+		fmt.Println("listById: %v\n", err)
+		return
+	}
+
+	encoder := json.NewEncoder(w)
+	encoder.SetIndent("", "    ")
+	err = encoder.Encode(&expr)
+	if err != nil {
+		fmt.Printf("listAll: %v\n", err)
+		return
+	}
 }
 
 func (ls *calcStates) sendTask(w http.ResponseWriter, r *http.Request) {
