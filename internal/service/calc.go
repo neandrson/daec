2c421939c1fc17f9fc44f428c3032e5ca3881cf9
vojan <serejah.mashichev@gmail.com>
2024-05-28 10:10:36 +0300

    Add calc service

---
+++ internal/service/calc.go
@@ -0,0 +1,94 @@
+package service
+
+import (
+	"fmt"
+	"slices"
+
+	"github.com/Vojan-Najov/daec/pkg/rpn"
+)
+
+const (
+	ErrorStatus     = "Error"
+	DoneStatus      = "Done"
+	InProcessStatus = "In process"
+)
+
+type Expression struct {
+	ID     string `json:"id"`
+	Status string `json:"status"`
+	Result string `json:"result"`
+	rpn    *rpn.RPN
+}
+
+type CalcService struct {
+	table map[string]Expression
+}
+
+// Структура для ответа по запросу на endpoint expressions/{id}
+type ExpressionUnit struct {
+	Expr Expression `json:"expression`
+}
+
+// Структура для ответа по запросу на endpoint expressions
+type ExpressionList struct {
+	Exprs []Expression `json:"expressions"`
+}
+
+func NewCalcService() *CalcService {
+	return &CalcService{
+		table: make(map[string]Expression),
+	}
+}
+
+func (cs *CalcService) AddExpression(id, expr string) error {
+	if len(id) == 0 {
+		return fmt.Errorf("empty ID")
+	}
+	if len(expr) == 0 {
+		return fmt.Errorf("empty expression")
+	}
+	if _, found := cs.table[id]; found {
+		return fmt.Errorf("not a unique ID: '%s'", id)
+	}
+
+	rpn, err := rpn.NewRPN(expr)
+	if err != nil {
+		return err
+	}
+
+	cs.table[id] = Expression{
+		ID:     id,
+		Status: InProcessStatus,
+		Result: "",
+		rpn:    rpn,
+	}
+
+	return nil
+}
+
+func (cs *CalcService) ListAll() ExpressionList {
+	lst := ExpressionList{}
+	for _, expr := range cs.table {
+		lst.Exprs = append(lst.Exprs, expr)
+	}
+
+	slices.SortFunc(lst.Exprs, func(a, b Expression) int {
+		if a.ID > b.ID {
+			return 1
+		} else if a.ID < b.ID {
+			return -1
+		}
+		return 0
+	})
+
+	return lst
+}
+
+func (cs *CalcService) FindById(id string) (ExpressionUnit, error) {
+	expr, found := cs.table[id]
+	fmt.Println(expr)
+	if !found {
+		return ExpressionUnit{}, fmt.Errorf("id %s no found", id)
+	}
+	return ExpressionUnit{Expr: expr}, nil
+}
