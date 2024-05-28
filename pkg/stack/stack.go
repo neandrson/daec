cfbdfd6edefc6ca3808b667a6ae2847edfe282a2
vojan <serejah.mashichev@gmail.com>
2024-05-28 10:12:17 +0300

    Add stack pkg

--- pkg/stack/stack.go
+++ pkg/stack/stack.go
@@ -1,12 +1,12 @@
 package stack
 
 type Stack[T any] struct {
-  arr []T
-  size int
+	arr  []T
+	size int
 }
 
 func NewStack[T any]() *Stack[T] {
-  return &Stack[T]{}
+	return &Stack[T]{}
 }
 
 func (s *Stack[T]) Empty() bool {
@@ -14,20 +14,19 @@ func (s *Stack[T]) Empty() bool {
 }
 
 func (s *Stack[T]) Top() T {
-	return s.arr[s.size - 1]
+	return s.arr[s.size-1]
 }
 
 func (s *Stack[T]) Push(val T) {
-  if cap(s.arr) == s.size {
-    s.arr = append(s.arr, val)
-  } else {
-    s.arr[s.size] = val
-  }
-  s.size++
+	if cap(s.arr) == s.size {
+		s.arr = append(s.arr, val)
+	} else {
+		s.arr[s.size] = val
+	}
+	s.size++
 }
 
 func (s *Stack[T]) Pop() T {
-  s.size--
-  return s.arr[s.size]
+	s.size--
+	return s.arr[s.size]
 }
-
--- pkg/stack/stack_test.go
+++ pkg/stack/stack_test.go
@@ -7,12 +7,11 @@ import (
 )
 
 func TestStack(t *testing.T) {
-    stack := stack.NewStack[int]()
-    stack.Push(1)
-    stack.Push(2)
-    popped := stack.Pop()
-    if popped != 2 {
-        t.Errorf("Expected popped element to be 2, but got %d", popped)
-    }
+	stack := stack.NewStack[int]()
+	stack.Push(1)
+	stack.Push(2)
+	popped := stack.Pop()
+	if popped != 2 {
+		t.Errorf("Expected popped element to be 2, but got %d", popped)
+	}
 }
-
