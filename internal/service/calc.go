87192424fa578a1a9bdba648ac6984f32d8450c6
vojan <serejah.mashichev@gmail.com>
2024-05-29 16:45:03 +0300

    Fix calc service

--- internal/service/calc.go
+++ internal/service/calc.go
@@ -1,49 +1,22 @@
 package service
 
 import (
+	"container/list"
 	"fmt"
 	"slices"
-
-	"github.com/Vojan-Najov/daec/pkg/rpn"
 )
-
-const (
-	ErrorStatus     = "Error"
-	DoneStatus      = "Done"
-	InProcessStatus = "In process"
-)
-
-type Expression struct {
-	ID     string `json:"id"`
-	Status string `json:"status"`
-	Result string `json:"result"`
-	rpn    *rpn.RPN
-}
 
 type CalcService struct {
-	table map[string]Expression
-}
-
-// Структура для ответа по запросу на endpoint expressions/{id}
-type ExpressionUnit struct {
-	Expr Expression `json:"expression`
-}
-
-// Структура для ответа по запросу на endpoint expressions
-type ExpressionList struct {
-	Exprs []Expression `json:"expressions"`
-}
-
-type Task struct {
-	Id        int64   `json:"id"`
-	Arg1      float64 `json:"arg1"`
-	Arg2      float64 `json:"arg1"`
-	Operation string  `json:"operation"`
+	table     map[string]*Expression
+	tasks     []*Task
+	taskID    int64
+	taskTable map[int64]*list.Element
 }
 
 func NewCalcService() *CalcService {
 	return &CalcService{
-		table: make(map[string]Expression),
+		table:     make(map[string]*Expression),
+		taskTable: make(map[int64]*list.Element),
 	}
 }
 
@@ -58,25 +31,19 @@ func (cs *CalcService) AddExpression(id, expr string) error {
 		return fmt.Errorf("not a unique ID: '%s'", id)
 	}
 
-	rpn, err := rpn.NewRPN(expr)
+	expression, err := NewExpression(id, expr)
 	if err != nil {
 		return err
 	}
 
-	cs.table[id] = Expression{
-		ID:     id,
-		Status: InProcessStatus,
-		Result: "",
-		rpn:    rpn,
-	}
-
+	cs.table[id] = expression
 	return nil
 }
 
 func (cs *CalcService) ListAll() ExpressionList {
 	lst := ExpressionList{}
 	for _, expr := range cs.table {
-		lst.Exprs = append(lst.Exprs, expr)
+		lst.Exprs = append(lst.Exprs, *expr)
 	}
 
 	slices.SortFunc(lst.Exprs, func(a, b Expression) int {
@@ -96,16 +63,61 @@ func (cs *CalcService) FindById(id string) (*ExpressionUnit, error) {
 	if !found {
 		return nil, fmt.Errorf("id %s no found", id)
 	}
-	return &ExpressionUnit{Expr: expr}, nil
+	return &ExpressionUnit{Expr: *expr}, nil
 }
 
-/*
-func (cs *CalcService) GetTask() (*Task) {
-	for k, v := range cs.table {
-		rpn := &v.rpn
-		for e := l.Front(); e != nil; e = e.Next() {
-			string(e)
+func (cs *CalcService) GetTask() *Task {
+	size := cs.updateTasks()
+	if size == 0 {
+		return nil
+	}
+
+	task := cs.tasks[0]
+	cs.tasks = cs.tasks[1:]
+	return task
+}
+
+func (cs *CalcService) updateTasks() int {
+	fmt.Println("size of tasks = ", len(cs.tasks))
+	if len(cs.tasks) > 0 {
+		return len(cs.tasks)
+	}
+	for i, expr := range cs.table {
+		fmt.Println("iteration #", i)
+		el := expr.Front()
+		for el != nil {
+			el1 := el
+			fmt.Println(el1.Value.(Token).Type())
+			if el1.Value.(Token).Type() != TokenTypeNumber {
+				el = el.Next()
+				continue
+			}
+			el2 := el1.Next()
+			fmt.Println(el2.Value.(Token).Type())
+			if el2 == nil || el2.Value.(Token).Type() != TokenTypeNumber {
+				el = el.Next()
+				continue
+			}
+			op := el2.Next()
+			if op == nil || op.Value.(Token).Type() != TokenTypeOperation {
+				el = el.Next()
+				continue
+			}
+			fmt.Println("tut")
+			task := new(Task)
+			task.ID = cs.taskID
+			cs.taskID++
+			taskToken := TaskToken{ID: task.ID}
+			cs.taskTable[task.ID] = expr.InsertBefore(&taskToken, el)
+			task.Arg1 = el1.Value.(NumToken).Value
+			task.Arg2 = el2.Value.(NumToken).Value
+			task.Operation = op.Value.(OpToken).Value
+			cs.tasks = append(cs.tasks, task)
+			el = op.Next()
+			expr.Remove(el1)
+			expr.Remove(el2)
+			expr.Remove(op)
 		}
 	}
+	return len(cs.tasks)
 }
-*/
