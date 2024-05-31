07f38903e495632d4036f28061ad9c4cd478e7cd
vojan <serejah.mashichev@gmail.com>
2024-05-31 17:32:19 +0300

    Fix calc service

--- internal/service/calc.go
+++ internal/service/calc.go
@@ -3,29 +3,30 @@ package service
 import (
 	"fmt"
 	"slices"
-	"time"
 	"sync"
+	"time"
 
 	"github.com/Vojan-Najov/daec/internal/orchestrator/config"
 	"github.com/Vojan-Najov/daec/internal/task"
-	"github.com/Vojan-Najov/daec/internal/result"
+	"github.com/Vojan-Najov/daec/pkg/timeout"
 )
 
 type CalcService struct {
-	locker    sync.RWMutex
-	exprTable map[string]*Expression
-	taskID    int64
-	tasks     []*task.Task
-	//taskTable map[int64]*list.Element
-	taskTable map[int64]ExprElement
-	timeTable map[string]time.Duration
+	locker        sync.RWMutex
+	exprTable     map[string]*Expression
+	taskID        int64
+	tasks         []*task.Task
+	taskTable     map[int64]ExprElement
+	timeTable     map[string]time.Duration
+	timeoutsTable map[int64]*timeout.Timeout
 }
 
 func NewCalcService(cfg config.Config) *CalcService {
 	cs := CalcService{
-		exprTable: make(map[string]*Expression),
-		taskTable: make(map[int64]ExprElement),
-		timeTable: make(map[string]time.Duration),
+		exprTable:     make(map[string]*Expression),
+		taskTable:     make(map[int64]ExprElement),
+		timeTable:     make(map[string]time.Duration),
+		timeoutsTable: make(map[int64]*timeout.Timeout),
 	}
 	cs.timeTable["+"] = cfg.Addtime
 	cs.timeTable["-"] = cfg.Subtime
@@ -99,92 +100,68 @@ func (cs *CalcService) GetTask() *task.Task {
 		return nil
 	}
 
-	task := cs.tasks[0]
+	newtask := cs.tasks[0]
 	cs.tasks = cs.tasks[1:]
-	return task
+
+	cs.timeoutsTable[newtask.ID] = timeout.NewTimeout(
+		5*time.Second + newtask.OperationTime,
+	)
+
+	go func(task task.Task) {
+		timeout, found := cs.timeoutsTable[task.ID]
+		if !found {
+			return
+		}
+
+		select {
+		case <-timeout.Timer.C:
+			cs.locker.Lock()
+			cs.locker.Unlock()
+			cs.tasks = append(cs.tasks, &task)
+		case <-timeout.Ctx.Done():
+			return
+		}
+
+	}(*newtask)
+
+	return newtask
 }
 
-func (cs *CalcService) PutResult(res result.Result) error {
+func (cs *CalcService) PutResult(id int64, value float64) error {
 	cs.locker.Lock()
 	defer cs.locker.Unlock()
 
-	_, found := cs.taskTable[res.ID]
-	if !found {
-		return fmt.Errorf("Task id %d not found", res.ID)
+	timeout, found := cs.timeoutsTable[id]
+	if found {
+		timeout.Ctx.Done()
 	}
 
-	fmt.Println(res)
+	_, found = cs.taskTable[id]
+	if !found {
+		return fmt.Errorf("Task id %d not found", id)
+	}
 
-	el := cs.taskTable[res.ID].Ptr
-	exprID := cs.taskTable[res.ID].ID
-	delete(cs.taskTable, res.ID)
+	el := cs.taskTable[id].Ptr
+	exprID := cs.taskTable[id].ID
+	delete(cs.taskTable, id)
 	expr, found := cs.exprTable[exprID]
 	if !found {
-		return fmt.Errorf("Expression for task %d not found", res.ID)
+		return fmt.Errorf("Expression for task %d not found", id)
 	}
-	
-	fmt.Println("len = ", expr.Len())
 
 	if expr.Len() == 1 {
-		expr.Result = fmt.Sprintf("%g", res.Value)
+		expr.Result = fmt.Sprintf("%g", value)
 		expr.Status = StatusDone
 		expr.Remove(el)
 	} else {
-		numToken := NumToken{res.Value}
+		numToken := NumToken{value}
 		expr.InsertBefore(numToken, el)
 		expr.Remove(el)
-		cs.extractTasksFromExpression(expr)		
+		cs.extractTasksFromExpression(expr)
 	}
 
 	return nil
 }
-
-/*
-func (cs *CalcService) updateTasks() int {
-	fmt.Println("size of tasks = ", len(cs.tasks))
-	if len(cs.tasks) > 0 {
-		return len(cs.tasks)
-	}
-	for i, expr := range cs.table {
-		fmt.Println("iteration #", i)
-		el := expr.Front()
-		for el != nil {
-			el1 := el
-			fmt.Println(el1.Value.(Token).Type())
-			if el1.Value.(Token).Type() != TokenTypeNumber {
-				el = el.Next()
-				continue
-			}
-			el2 := el1.Next()
-			fmt.Println(el2.Value.(Token).Type())
-			if el2 == nil || el2.Value.(Token).Type() != TokenTypeNumber {
-				el = el.Next()
-				continue
-			}
-			op := el2.Next()
-			if op == nil || op.Value.(Token).Type() != TokenTypeOperation {
-				el = el.Next()
-				continue
-			}
-			task := new(task.Task)
-			task.ID = cs.taskID
-			cs.taskID++
-			taskToken := TaskToken{ID: task.ID}
-			cs.taskTable[task.ID] = expr.InsertBefore(&taskToken, el)
-			task.Arg1 = el1.Value.(NumToken).Value
-			task.Arg2 = el2.Value.(NumToken).Value
-			task.Operation = op.Value.(OpToken).Value
-			task.Time = cs.timeTable[task.Operation]
-			cs.tasks = append(cs.tasks, task)
-			el = op.Next()
-			expr.Remove(el1)
-			expr.Remove(el2)
-			expr.Remove(op)
-		}
-	}
-	return len(cs.tasks)
-}
-*/
 
 func (cs *CalcService) extractTasksFromExpression(expr *Expression) int {
 	var taskCount int
@@ -215,8 +192,8 @@ func (cs *CalcService) extractTasksFromExpression(expr *Expression) int {
 		taskToken := TaskToken{ID: task.ID}
 		taskElement := expr.InsertBefore(&taskToken, el)
 		cs.taskTable[task.ID] = ExprElement{expr.ID, taskElement}
-		task.Arg1 = el1.Value.(NumToken).Value
-		task.Arg2 = el2.Value.(NumToken).Value
+		task.Arg1 = fmt.Sprintf("%f", el1.Value.(NumToken).Value)
+		task.Arg2 = fmt.Sprintf("%f", el2.Value.(NumToken).Value)
 		task.Operation = op.Value.(OpToken).Value
 		task.OperationTime = cs.timeTable[task.Operation]
 
