7f550e8b795637fbecf478bc519ddf2ce52de7fa
vojan <serejah.mashichev@gmail.com>
2024-05-31 17:32:56 +0300

    Fix task struct

--- internal/task/task.go
+++ internal/task/task.go
@@ -4,8 +4,8 @@ import "time"
 
 type Task struct {
 	ID            int64         `json:"id"`
-	Arg1          float64       `json:"arg1"`
-	Arg2          float64       `json:"arg2"`
+	Arg1          string        `json:"arg1"`
+	Arg2          string        `json:"arg2"`
 	Operation     string        `json:"operation"`
 	OperationTime time.Duration `json:"operation_time"`
 }
